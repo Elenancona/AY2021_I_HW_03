ba6a9d637a7a93f733ad7c8b9346bfc09d90cf76
EleAnc <elena.ancona@mail.polimi.it>
2020-10-15 20:12:18 +0200

    Modified topDsign

---
+++ ANCONA_ELENA.cydsn/InterruptRoutines_UART.c
@@ -0,0 +1,26 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+#include "InterruptRoutines_UART.h"
+#include "stdio.h"
+#include "UART.h"
+
+
+CY_ISR(Custom_UART_IRS)
+{
+    
+    
+    
+}
+
+
+/* [] END OF FILE */
---
+++ ANCONA_ELENA.cydsn/InterruptRoutines_UART.h
@@ -0,0 +1,21 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#ifndef __INTERRUPT_ROUTINES_UART__
+    #define __INTERRUPT_ROUTINES_UART__
+    
+    #include "cytypes.h"
+    
+    CY_ISR_PROTO(Custom_UART_IRS);
+    
+#endif
+
+/* [] END OF FILE */
